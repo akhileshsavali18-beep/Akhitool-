5154f563e5d752daacf1d6c32dc8ed2dae6795ef
akhileshsavali18-beep <akhileshsavali18@gmail.com>
2026-07-14 05:35:52 +0530

    Create firebase-config.js

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,25 @@
+// assets/js/firebase-config.js
+
+/* 
+ * FIREBASE READY CONFIGURATION
+ * 
+ * Instructions:
+ * 1. Go to console.firebase.google.com
+ * 2. Create a new project
+ * 3. Add a web app to the project
+ * 4. Copy the config object below and replace the empty strings
+ * 5. Import Firebase SDKs in your HTML files when ready
+ */
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+// NOTE: Firebase initialization is disabled until credentials are added.
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.x.x/firebase-app.js";
+// const app = initializeApp(firebaseConfig);
